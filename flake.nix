9fb6268926eea957daedf08f78f7dd38a4bd832e
SpiralP <spiral.palmer@gmail.com>
2026-05-31 02:38:22 -0700

    ci: remove crates.io fix

--- flake.nix
+++ flake.nix
@@ -8,29 +8,6 @@
     flake-utils.lib.makeOutputs inputs
       ({ lib, pkgs, makeRustPackage, dev, ... }:
         let
-          # TODO remove when https://github.com/NixOS/nixpkgs/pull/524985 is
-          # merged into nixos-25.11. crates.io's API rate-limits nix's curl
-          # to 1 req/s and returns 403s above that; the upstream fix swaps
-          # the download URL to the static.crates.io CDN. Inline the same
-          # one-line change here until the backport lands.
-          patchedImportCargoLock =
-            let
-              cargoLockDir = "${pkgs.path}/pkgs/build-support/rust";
-            in
-            pkgs.callPackage
-              (builtins.toFile "import-cargo-lock-patched.nix"
-                (builtins.replaceStrings
-                  [
-                    "https://crates.io/api/v1/crates"
-                    "./replace-workspace-values.py"
-                  ]
-                  [
-                    "https://static.crates.io/crates"
-                    "${cargoLockDir}/replace-workspace-values.py"
-                  ]
-                  (builtins.readFile "${cargoLockDir}/import-cargo-lock.nix")))
-              { };
-
           args = {
             src = ./.;
 
@@ -40,11 +17,6 @@
             ];
 
             useNextest = true;
-
-            cargoDeps = patchedImportCargoLock {
-              lockFile = ./Cargo.lock;
-              allowBuiltinFetchGit = true;
-            };
           };
         in
         {
